e7adaea129da5a75c83b98b52f86ea3ec054358d
ladajinkya <47538650+ladajinkya@users.noreply.github.com>
2020-04-24 14:59:59 +0530

    Local Notification

--- LocalNotification/AppDelegate.swift
+++ LocalNotification/AppDelegate.swift
@@ -1,53 +1,100 @@
-//
-//  AppDelegate.swift
-//  LocalNotification
-//
-//  Created by Mac on 24/04/20.
-//  Copyright © 2020 Mac. All rights reserved.
-//
 
 import UIKit
 import CoreData
-
+import UserNotifications
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+class AppDelegate: UIResponder, UIApplicationDelegate,UNUserNotificationCenterDelegate {
+    var window : UIWindow?
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        UNUserNotificationCenter.current().delegate = self
         // Override point for customization after application launch.
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        print("------------------------.")
+        
     }
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                    didReceive response: UNNotificationResponse,
+                                    withCompletionHandler completionHandler: @escaping () -> Void) {
+            
+            
+            
+            let application = UIApplication.shared
+            
+            if(application.applicationState == .active){
+              print("user tapped the notification bar when the app is in foreground")
+                if response.notification.request.identifier == "YOUR_UNIQUE_IDENTIFIER" {
+                           print("handling notifications with the YOUR_UNIQUE_IDENTIFIER Identifier")
+                       }
+                
+        }
+            if(application.applicationState == .inactive)
+            {
+              print("user tapped the notification bar when the app is  inactive")
+                if response.notification.request.identifier == "YOUR_UNIQUE_IDENTIFIER" {
+                           print("handling notifications with the YOUR_UNIQUE_IDENTIFIER Identifier")
+                       }
+            }
+            
+            if(application.applicationState == .background)
+            {
+              print("user tapped the notification bar when the app is in background")
+                if response.notification.request.identifier == "YOUR_UNIQUE_IDENTIFIER" {
+                           print("handling notifications with the YOUR_UNIQUE_IDENTIFIER Identifier")
+                       }
+            }
+        else
+            {
+                print("NO State")
+        }
+            completionHandler()
+        }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
+    func applicationWillResignActive(_ application: UIApplication) {
+       UNUserNotificationCenter.current().delegate = self
+           print("Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.") 
+           // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+       }
 
+       func applicationDidEnterBackground(_ application: UIApplication) {
+        UNUserNotificationCenter.current().delegate = self
+           print(" Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.")
+           // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+       }
+
+       func applicationWillEnterForeground(_ application: UIApplication) {
+        UNUserNotificationCenter.current().delegate = self
+           print("Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.")
+       }
+
+       func applicationDidBecomeActive(_ application: UIApplication) {
+            print("Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.")
+       }
+
+       func applicationWillTerminate(_ application: UIApplication) {
+            print("Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.")
+       }
+
+    
+    
     // MARK: - Core Data stack
-
+    
     lazy var persistentContainer: NSPersistentContainer = {
         /*
          The persistent container for the application. This implementation
          creates and returns a container, having loaded the store for the
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
-        */
+         */
         let container = NSPersistentContainer(name: "LocalNotification")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                 
+                
                 /*
                  Typical reasons for an error here include:
                  * The parent directory does not exist, cannot be created, or disallows writing.
@@ -61,9 +108,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         })
         return container
     }()
-
+    
     // MARK: - Core Data Saving support
-
+    
     func saveContext () {
         let context = persistentContainer.viewContext
         if context.hasChanges {
@@ -77,6 +124,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
     }
-
+    
 }
 
--- LocalNotification/ViewController.swift
+++ LocalNotification/ViewController.swift
@@ -1,20 +1,65 @@
-//
-//  ViewController.swift
-//  LocalNotification
-//
-//  Created by Mac on 24/04/20.
-//  Copyright © 2020 Mac. All rights reserved.
-//
+
 
 import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+import UserNotifications
+class ViewController: UIViewController,UNUserNotificationCenterDelegate {
+    let content = UNMutableNotificationContent()
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
-
-
+    
+    
+    @IBAction func registerBtnAction(_ sender: Any)
+    {
+        self.grantAccess()
+    }
+    @IBAction func sendNotification(_ sender: Any) {
+        self.setpNotificationwithContent()
+    }
+    func grantAccess()
+    {
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert]) {
+            (granted, error) in
+            if granted {
+                print("Granted")
+            } else {
+                print("Denied")
+            }
+        }
+    }
+    func setpNotificationwithContent()
+    {
+        var dateString = String()
+        dateString = "2020-04-24 14:37:00"
+        let date = self.StringToDate(dateStr: dateString)
+        print("date = \(date)")
+        
+        content.title = "Hello viewers"
+        content.body = "This is noitifcation body"
+        content.sound = UNNotificationSound.default
+        
+        let triggerDate = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date)
+        print("trigger date = \(triggerDate)")
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: triggerDate, repeats: false)
+        let request = UNNotificationRequest.init(identifier: "YOUR_UNIQUE_IDENTIFIER", content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: { (error) in
+            if let error = error {
+                print(error)
+            }
+        })
+    }
+    func StringToDate(dateStr: String) -> Date {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        dateFormatter.timeZone = TimeZone.current
+        let dateFromString = dateFormatter.date(from: dateStr)
+        print("dateFromString: ", dateFromString!)
+        return dateFromString!
+    }
 }
 
